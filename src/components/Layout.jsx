8424dcf4e10eb9a5486d91d3394f43ca51d4c6ab
Martti-R <Martti-R@users.noreply.github.com>
2017-05-12 15:40:34 +0300

    Router for showing/hiding elements. Controlled form components for adding new and editing existing book items.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,53 +5,137 @@ import BookStore from '../stores/BookStore.js';
 import * as BookActions from '../actions/BookActions.js';
 import Book from './Book.jsx';
 
+import SaveForm from './SaveForm.jsx';
+import EditForm from './EditForm.jsx';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const AddNewBook = () => (
+  <SaveForm />
+)
+
+const PageLinks = () => (
+  <div>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/book/add">Add New Book</Link></li>
+    </ul>
+
+    <hr/>
+    <Route path="/book/add" exact={true} component={AddNewBook}/>
+  </div>
+)
+
 export default class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    this.getPageLinks = this.getPageLinks.bind(this);
+
+    this.getBookEditComponent = this.getBookEditComponent.bind(this);
+    this.getHomeComponent = this.getHomeComponent.bind(this);
+
     this.getAllBooks = this.getAllBooks.bind(this); // Must be
-    this.state = {
-      books : BookStore.getAll(),
-      name: "",
-      fetchStatus: "Fetching books..."
-    }
+    this.getBookLinks = this.getBookLinks.bind(this);
+    this.handleBookEditInputChange = this.handleBookEditInputChange.bind(this);
+    this.handleBookEditSubmit = this.handleBookEditSubmit.bind(this);
+    this.state = {books: []}
   }
 
   // Kuunnellaan change eventtiä. Kun tulee, haetaan kaikki kirjat
   componentWillMount() {
-    BookStore.on("change", this.getAllBooks)
+    BookStore.on("change", this.getAllBooks);
     BookActions.getBooks();
     console.log("Number", BookStore.listenerCount("change"));
+
   }
-  
+
   componentWillUnmount() {
-    BookStore.removeListener("change", this.getAllBooks)
+    BookStore.removeListener("change", this.getAllBooks);
   }
 
   getAllBooks() {
-    this.setState({books: BookStore.getAll()})
-    this.setState({fetchStatus: BookStore.fetchStatus})
+
+    this.setState({books: BookStore.getAll()});
+    this.setState({fetchStatus: BookStore.fetchStatus});
+
   }
 
-  addBook() {
-    BookActions.addBook({id: Date.now(), name: this.state.name, author: "Me", description: "cc"})
-    this.state.name = "";
+  handleBookEditInputChange(bookId, fieldName, value) {
+
+    const bookIndex = this.state.books.findIndex((book) => book.id == bookId);
+
+    if (bookIndex != -1) {
+      let booksData = this.state.books;
+      let editedBookData = booksData.filter((book) => book.id == bookId)[0];
+      editedBookData[fieldName] = value;
+      booksData[bookIndex] = editedBookData;
+      this.setState({books: booksData});
+    } else {
+      console.error(`Cannot find book with id ${bookId}`);
+    }
+
   }
 
-  getBooks() {
-    BookActions.getBooks();
+  handleBookEditSubmit(bookId) {
+    console.log('edit submit of book ' + bookId);
   }
 
-  render() {
-    const mappedBooks = this.state.books.map(z => <Book key={z.id} name={z.name} author={z.author} />)
+  getPageLinks() {
     return (
       <div>
-        <div>
-          {this.state.fetchStatus}
-          {mappedBooks}
-        </div>
-        <input value={this.state.name} onChange={(me) => this.setState({name: me.target.value})}></input>
-        <div><button onClick={this.addBook.bind(this)}>Add New</button></div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/book/add">Add New Book</Link></li>
+          </ul>
+
+          <hr/>
+          <Route path="/" component={this.getHomeComponent}/>
+          <Route path="/book/add" component={AddNewBook}/>
       </div>
+    )
+  }
+
+  getBookEditComponent({ match }) {
+    const booksById = this.state.books.filter((book) => book.id == match.params.id);
+    const bookProps = booksById[0];
+    return <EditForm {...bookProps} onBookEditChange={this.handleBookEditInputChange} onBookEditSubmit={this.handleBookEditSubmit} />
+  }
+
+  getHomeComponent() {
+    return (
+      <div>
+        {this.state.fetchStatus}
+        {this.getBookLinks()}
+      </div>
+    )
+  }
+
+  getBookLinks() {
+    const bookLinks = this.state.books.map(z =>
+      <li key={z.id}>
+        <Link to={`/book/edit/${z.id}`}>{z.name} by {z.author} (click to edit)</Link>
+      </li>
+    )
+    return (
+      <div>
+        <ul>
+          {bookLinks}
+        </ul>
+        <Route path="/book/edit/:id" component={this.getBookEditComponent} />
+      </div>
+    )
+  }
+
+  render() {    
+    return (
+      <Router>
+        {this.getPageLinks()}
+      </Router>
     )
   }
 }

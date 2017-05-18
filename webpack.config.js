b5e7c41d6e1b97ab7b0e7862645a00a281ff3517
Martti-R <Martti-R@users.noreply.github.com>
2017-05-18 13:46:06 +0300

    Updates for making it possible to use jest with snapshot testing

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,40 @@
 "use strict";
 var webpack = require('webpack');
 var path = require('path');
-var loaders = require('./webpack.loaders');
+var loaderRules = require('./webpack.loaders');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const HOST = process.env.HOST || "127.0.0.1";
 const PORT = process.env.PORT || "8888";
 
-// global css
-loaders.push({
-	test: /[\/\\](node_modules|global)[\/\\].*\.css$/,
-	loaders: [
-		'style?sourceMap',
-		'css'
-	]
-});
-// local scss modules
-loaders.push({
-	test: /[\/\\]src[\/\\].*\.scss/,
-	exclude: /(node_modules|bower_components|public)/,
-	loaders: [
-		'style?sourceMap',
-		'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
-		'sass'
-	]
-});
-
-// local css modules
-loaders.push({
-	test: /[\/\\]src[\/\\].*\.css/,
-	exclude: /(node_modules|bower_components|public)/,
-	loaders: [
-		'style?sourceMap',
-		'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
-	]
-});
-
 module.exports = {
-	entry: [
-		`webpack-dev-server/client?http://${HOST}:${PORT}`,
-		`webpack/hot/only-dev-server`,
-		`./src/index.jsx` // Your appʼs entry point
-	],
+	entry: {
+		'app': [
+			'react-hot-loader/patch',
+			`webpack-dev-server/client?http://${HOST}:${PORT}`,
+			`webpack/hot/only-dev-server`,
+			`./src/index.jsx` // Your appʼs entry point
+		]
+	},
 	devtool: process.env.WEBPACK_DEVTOOL || 'cheap-module-source-map',
 	output: {
 		path: path.join(__dirname, 'public'),
 		filename: 'bundle.js'
 	},
 	resolve: {
-		extensions: ['', '.js', '.jsx']
+		extensions: ['.js', '.jsx'],
+		modules: [
+			path.join(__dirname, "src"),
+			"node_modules"
+		]
 	},
 	module: {
-		loaders
+		rules: loaderRules
 	},
 	devServer: {
 		contentBase: "./public",
 		// do not print bundle build stats
 		noInfo: true,
-		// enable HMR
-		hot: true,
 		// embed the webpack-dev-server runtime into the bundle
 		inline: true,
 		// serve index.html in place of 404 responses to allow HTML5 history
@@ -67,10 +43,9 @@ module.exports = {
 		host: HOST
 	},
 	plugins: [
-		new webpack.NoErrorsPlugin(),
-		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoEmitOnErrorsPlugin(),
 		new HtmlWebpackPlugin({
 			template: './src/index.html'
 		}),
-	]
+	],
 };
